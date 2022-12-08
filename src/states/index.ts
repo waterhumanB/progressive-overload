672b2580dfeb140b42e0c54157fff451c3b52c25
waterhumanB <dpfflsk45@gmail.com>
2022-12-08 22:04:28 +0900

    feat: 리덕스 툴킷 설치 및 store 기본 셋팅

---
+++ src/states/index.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {},
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
