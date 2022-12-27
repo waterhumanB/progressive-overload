08b1ccbc226e9e986f7bb292a3d6bebc190ba4e9
waterhumanB <dpfflsk45@gmail.com>
2022-12-27 23:00:58 +0900

    feat: routine-ready 페이지 안에서 운동 한개 교체 시 모달 UI 기본 완성

--- src/components/Modal/ExerciseEditDeleteModal/index.tsx
+++ src/components/Modal/ExerciseEditDeleteModal/index.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import { useState, MouseEvent } from 'react'
 import { useAppDispatch } from '../../../hooks/useAppDispatch'
 import { useAppSelector } from '../../../hooks/useAppSelector'
 import { getExerciseData } from '../../../states/exercise'
 import { deleteExerciseInRoutine, getRoutineData } from '../../../states/routines'
+import { getTypesData } from '../../../states/types'
 import { IExerciseEditDeleteModalProps } from '../../../types/allProps.d'
+import { findCategory, findTarget, findType } from '../../../utils/findmenu'
+import { ReactComponent as Arrow } from '../../../assets/imgs/arrow.svg'
 import * as S from './styles'
 
 const ExerciseEditDeleteModal = ({
@@ -13,16 +16,18 @@ const ExerciseEditDeleteModal = ({
 }: IExerciseEditDeleteModalProps) => {
   const exerciseSelector = useAppSelector(getExerciseData)
   const routineSelector = useAppSelector(getRoutineData)
+  const typeSelector = useAppSelector(getTypesData)
   const dispatch = useAppDispatch()
   const [changeExercise, setChangeExercise] = useState(false)
-  // console.log(exerciseSelector.exercises.byId[nowExerciseIdData[1]])
-  // console.log(routineSelector.routines.byId)
+  const [recommendExerciseOrder, setRecommendExerciseOrder] = useState(0)
+  const recommendExerciseList = Object.values(exerciseSelector.exercises.byId).filter(
+    (data) => data.mainTarget === exerciseSelector.exercises.byId[nowExerciseIdData[1]].mainTarget
+  )
 
   const changeExerciseHanlder = () => {
     setChangeExercise(true)
   }
   const deleteExerciseHanlder = () => {
-    // toggleModalHandler()
     dispatch(
       deleteExerciseInRoutine({
         id: nowExerciseIdData[0],
@@ -31,21 +36,66 @@ const ExerciseEditDeleteModal = ({
     )
     toggleModalHandler()
   }
+
+  const recommendExerciseOrderLeftHandler = () => {
+    if (recommendExerciseOrder > 0) {
+      setRecommendExerciseOrder(recommendExerciseOrder - 1)
+    }
+  }
+
+  const recommendExerciseOrderRightHandler = () => {
+    if (recommendExerciseOrder < recommendExerciseList.length - 1) {
+      setRecommendExerciseOrder(recommendExerciseOrder + 1)
+    }
+  }
+  console.log(recommendExerciseOrder, recommendExerciseList.length)
   return (
     <S.exerciseEditDeleteModalContainer>
       {!changeExercise ? (
         <S.exerciseEditDeleteTitle>운동을 루틴에서 삭제할까요?</S.exerciseEditDeleteTitle>
       ) : (
-        <div>운동 바꿔용</div>
+        <S.selectExerciseBox>
+          <button
+            type='button'
+            className={recommendExerciseOrder === 0 ? 'left hidden' : 'left'}
+            onClick={recommendExerciseOrderLeftHandler}
+          >
+            <Arrow />
+          </button>
+          {recommendExerciseList.slice(recommendExerciseOrder, recommendExerciseOrder + 1).map((data) => (
+            <S.selectExercise key={data.id}>
+              <div className='title'>
+                <div>{`${findCategory(data.categoryId)} ${findType(typeSelector.types.byId, data.typeId)}`}</div>
+              </div>
+              <div className='target'>
+                <div>{`${findTarget(data.mainTarget)} ${findTarget(data.secondaryTarget)}`}</div>
+              </div>
+            </S.selectExercise>
+          ))}
+
+          <button
+            type='button'
+            className={recommendExerciseOrder === recommendExerciseList.length - 1 ? ' hidden' : ''}
+            onClick={recommendExerciseOrderRightHandler}
+          >
+            <Arrow />
+          </button>
+        </S.selectExerciseBox>
       )}
-      <S.exerciseEditDeleteBtnBox>
-        <button type='button' onClick={changeExerciseHanlder}>
-          다른 운동 대체
-        </button>
-        <button type='button' onClick={deleteExerciseHanlder}>
-          운동 삭제하기
-        </button>
-      </S.exerciseEditDeleteBtnBox>
+      {!changeExercise ? (
+        <S.exerciseEditDeleteBtnBox>
+          <button type='button' onClick={changeExerciseHanlder}>
+            다른 운동 대체
+          </button>
+          <button type='button' onClick={deleteExerciseHanlder}>
+            운동 삭제하기
+          </button>
+        </S.exerciseEditDeleteBtnBox>
+      ) : (
+        <S.exerciseEditDeleteBtnBox>
+          <button type='button'>운동 대체 하기</button>
+        </S.exerciseEditDeleteBtnBox>
+      )}
     </S.exerciseEditDeleteModalContainer>
   )
 }
--- src/components/Modal/ExerciseEditDeleteModal/styles.ts
+++ src/components/Modal/ExerciseEditDeleteModal/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import colors from '../../../styles/constants/colors'
 
 export const exerciseEditDeleteModalContainer = styled.div`
@@ -38,5 +38,65 @@ export const exerciseEditDeleteBtnBox = styled.div`
       background: ${colors.FOCUS};
       border: 1px solid ${colors.FOCUS};
     }
+  }
+`
+export const selectExerciseBox = styled.div`
+  width: 100%;
+  display: flex;
+  margin: auto;
+
+  button {
+    margin: auto;
+    svg {
+      width: 30px;
+      height: 40px;
+      fill: ${colors.FONT};
+    }
+    :hover {
+      svg {
+        fill: ${colors.FOCUS};
+      }
+    }
+  }
+  .left {
+    svg {
+      transform: scaleX(-1);
+    }
+  }
+  .hidden {
+    opacity: 0;
+    cursor: default;
+  }
+`
+const animation = keyframes`
+0% { transform: scaleY(0.1)}
+100% { transform: scaleY(1)}
+`
+
+export const selectExercise = styled.div`
+  width: 100%;
+  padding: 15px 0;
+  margin: auto;
+  animation-name: ${animation};
+  animation-duration: 0.5;
+
+  div {
+    display: flex;
+    justify-content: center;
+  }
+  .title {
+    font-size: 20px;
+    text-align: center;
+    color: ${colors.FOCUS};
+    font-weight: 600;
+    :nth-child(0) {
+      margin-right: 5px;
+    }
+  }
+
+  .target {
+    margin-top: 10px;
+    color: ${colors.FONT};
+    font-weight: 600;
   }
 `
