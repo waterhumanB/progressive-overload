4937f16b4e0a9cf5aa47a1c3fa57f3f6ecd7e70b
waterhumanB <dpfflsk45@gmail.com>
2023-01-04 22:54:06 +0900

    feat: record 데이터 mouseEvent로 좌우로 드래그 기능 완료

--- src/components/Routine/CurrentExerciseRecordCard/index.tsx
+++ src/components/Routine/CurrentExerciseRecordCard/index.tsx
@@ -1,4 +1,5 @@
 import * as S from './styles'
+import { MouseEvent, useRef, useState } from 'react'
 import { IExerciseItem } from '../../../types/exercises.d'
 import { useAppSelector } from '../../../hooks/useAppSelector'
 import { getRecordsData } from '../../../states/records'
@@ -10,29 +11,69 @@ interface ICurrentExerciseDataProp {
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 const CurrentExerciseRecordCard = ({ currentExerciseData }: ICurrentExerciseDataProp) => {
+  const [startPageX, setStartPageX] = useState(0)
+  const [translateX, setTranslateX] = useState(0)
+  const [isMouseEvent, setIsMoseEvent] = useState(false)
+  const mouseRef = useRef<HTMLDivElement>(null)
   const recordSelector = useAppSelector(getRecordsData)
   const currentExerciseRecords = Object.values(recordSelector.records.byId).filter((data) =>
     currentExerciseData.record.includes(data.id)
   )
-  const fetchedDay = (date: string) => {
+  const fetchedDate = (date: string) => {
     const year = date.split(' ')[3]
     const month = months.indexOf(date.split(' ')[1]) + 1
     const day = date.split(' ')[2]
     const fetchedMonth = month < 10 ? `0${month}` : month
-    return `${year}-${fetchedMonth}-${day}`
+    return `${year}.${fetchedMonth}.${day}`
   }
+  const mouseDownHandler = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsMoseEvent(true)
+    setStartPageX(e.pageX - translateX)
+  }
+  const mouseMoveHandler = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    const deltaX = e.pageX - startPageX
+    if (isMouseEvent) {
+      setTranslateX(deltaX)
+    }
+    if (-e.currentTarget.offsetWidth * 1.3 >= translateX || e.currentTarget.offsetWidth / 2 <= translateX) {
+      setIsMoseEvent(false)
+      setTranslateX(0)
+    }
+  }
+  const mouseUpHandler = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsMoseEvent(false)
+  }
+  const mouseLeaveHandler = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsMoseEvent(false)
+  }
+  console.log('transX', translateX)
   return (
-    <S.recordCardContainer>
-      <div>이전기록</div>
-      <S.recordCardbox>
+    <S.recordCardContainer ref={mouseRef}>
+      <div className='title'>이전 기록</div>
+      <S.recordCardbox
+        onMouseDown={mouseDownHandler}
+        onMouseMove={mouseMoveHandler}
+        onMouseUp={mouseUpHandler}
+        onMouseLeave={mouseLeaveHandler}
+      >
         {currentExerciseRecords.map((data) => (
-          <S.cardItem key={data.startAt}>
-            <div>{fetchedDay(data.startAt)}</div>
+          <S.cardItem translateX={translateX} key={data.startAt}>
+            <S.cardDate>{fetchedDate(data.startAt)}</S.cardDate>
             {data.set.map((item) => (
               <S.setItem key={item.order}>
-                <div>{item.order}-</div>
+                <div className='order'>
+                  {item.order}
+                  <span className='dash'> -</span>
+                </div>
                 <div>
-                  {item.kg}: kg {item.rab}: rab
+                  {item.kg}
+                  <span className='kgAndRab'>KG</span>
+                  {item.rab}
+                  <span className='kgAndRab'>회</span>
                 </div>
               </S.setItem>
             ))}
--- src/components/Routine/CurrentExerciseRecordCard/styles.ts
+++ src/components/Routine/CurrentExerciseRecordCard/styles.ts
@@ -1,22 +1,56 @@
 import styled from 'styled-components'
+import colors from '../../../styles/constants/colors'
 
 export const recordCardContainer = styled.div`
   width: 100%;
+  margin: 10px auto;
+  .title {
+    margin-bottom: 10px;
+    color: ${colors.FOCUS};
+    font-size: 18px;
+    font-weight: 600;
+  }
 `
 
 export const recordCardbox = styled.div`
   width: 100%;
-  height: 120px;
+  height: 100%;
   overflow: auto;
   white-space: nowrap;
   display: flex;
 `
 
-export const cardItem = styled.div`
+export const cardDate = styled.div`
   width: 100%;
+  color: ${colors.BLACK};
+  font-weight: 600;
+  margin-bottom: 5px;
+`
+
+export const cardItem = styled.div<{ translateX: number }>`
+  width: 100%;
+  text-align: center;
+  transform: ${(props) => `translateX(${props.translateX}px)`};
 `
 
 export const setItem = styled.div`
   width: 100%;
   display: flex;
+  justify-content: center;
+  font-weight: 600;
+  font-size: 18px;
+
+  .order {
+    margin-right: 5px;
+
+    .dash {
+      color: ${colors.RED};
+    }
+  }
+
+  .kgAndRab {
+    margin: auto 3px;
+    font-size: 15px;
+    color: ${colors.FONT};
+  }
 `
