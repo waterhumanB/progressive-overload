121bf2779dec93922a6712546ea8db265fd17d82
waterhumanB <dpfflsk45@gmail.com>
2023-01-18 23:50:04 +0900

    feat: 요약페이지 막대 그래프 기본 UI 구현

--- src/routes/Summary/index.tsx
+++ src/routes/Summary/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import DountChart from '../../components/Chart/DountChart'
 import Footer from '../../components/Footer'
 import { useAppSelector } from '../../hooks/useAppSelector'
@@ -8,10 +8,9 @@ import { getRoutineData } from '../../states/routines'
 import * as S from './styles'
 
 const ONE_YEAR_AVERAGE_EXERCISE_DAY = 188
-const ONE_YEAR_AVERAGE_EXERCISE_HOUR = 187
-const ONE_YEAR_AVERAGE_EXERCISE_VOLUME = 250
+const ONE_YEAR_AVERAGE_EXERCISE_HOUR = 187 // 일주일 3.5 한달 15.5
+const ONE_YEAR_AVERAGE_EXERCISE_VOLUME = 250 // 일주일 4.8 한달 20
 const Summary = () => {
-  const thisYear = new Date().getFullYear
   const routineSelector = useAppSelector(getRoutineData)
   const recordSelector = useAppSelector(getRecordsData)
 
@@ -25,12 +24,13 @@ const Summary = () => {
     })
     .reduce((acc, el) => acc + el)
 
-  const execiseAllDates = Object.values(recordSelector.records.byId).map((data) => {
-    const workoutDays = data.startAt.split(' ')[0] + data.startAt.split(' ')[1] + data.startAt.split(' ')[2]
+  const exerciseAllDates = Object.values(recordSelector.records.byId).map((data) => {
+    const workoutDays = `${data.startAt.split(' ')[0]} ${data.startAt.split(' ')[1]} ${data.startAt.split(' ')[2]}`
     return workoutDays
   })
 
-  const totalWorkoutDays = execiseAllDates.filter((data, idx) => execiseAllDates.indexOf(data) === idx).length
+  const totalWorkoutDays = exerciseAllDates.filter((data, idx) => exerciseAllDates.indexOf(data) === idx).length
+
   const totalExercisedVolume = Object.values(recordSelector.records.byId)
     .map((data) => {
       return data.set.map((item) => item.kg * item.rab)
@@ -38,10 +38,12 @@ const Summary = () => {
     .flat(1)
     .reduce((acc, el) => acc + el)
 
+  console.log(exerciseAllDates)
+  console.log(Object.values(recordSelector.records.byId))
   return (
     <section>
       <div>나의 운동 기록들</div>
-      <S.chartConatiner>
+      <S.dountChartContainer>
         <DountChart
           percentage={ONE_YEAR_AVERAGE_EXERCISE_HOUR}
           percentageValue={totalTimeExercised}
@@ -57,16 +59,47 @@ const Summary = () => {
           percentageValue={totalWorkoutDays}
           chartValueName='Days'
         />
-      </S.chartConatiner>
+      </S.dountChartContainer>
       <div>
-        <div>일간</div>
-        <div>주간</div>
-        <div>월간</div>
+        <button type='button'>일간</button>
+        <button type='button'>주간</button>
+        <button type='button'>월간</button>
       </div>
-      <div>그래프</div>
+      <S.barChartContainer>
+        <S.yAxis>
+          <div>170</div>
+          <div>130</div>
+          <div>80</div>
+          <div>40</div>
+          <div>0</div>
+        </S.yAxis>
+        <svg viewBox='0 0 400 250'>
+          <rect x='10' y={`${200 - 190}`} width='10' height='190' stroke='black' strokeWidth='3px' />
+          <text x='0' y='240'>
+            1/18
+          </text>
+          <text x='8' y='220'>
+            수
+          </text>
+          <rect x='60' y={`${200 - 150}`} width='10' height='150' stroke='black' strokeWidth='3px' />
+          <text x='52' y='220'>
+            150
+          </text>
+          <rect x='110' y={`${200 - 110}`} width='10' height='110' stroke='black' strokeWidth='3px' />
+          <rect x='160' y={`${200 - 80}`} width='10' height='80' stroke='black' strokeWidth='3px' />
+          <rect x='210' y={`${200 - 50}`} width='10' height='50' stroke='black' strokeWidth='3px' />
+          <rect x='260' y={`${200 - 90}`} width='10' height='90' stroke='black' strokeWidth='3px' />
+          <rect x='310' y={`${200 - 150}`} width='10' height='150' stroke='black' strokeWidth='3px' />
+          <rect x='360' y={`${200 - 70}`} width='10' height='70' stroke='black' strokeWidth='3px' />
+          <rect x='410' y={`${200 - 50}`} width='10' height='50' stroke='black' strokeWidth='3px' />
+          <rect x='460' y={`${200 - 140}`} width='10' height='140' stroke='black' strokeWidth='3px' />
+          <rect x='510' y={`${200 - 120}`} width='10' height='120' stroke='black' strokeWidth='3px' />
+        </svg>
+      </S.barChartContainer>
+
       <div>
-        <div>총 볼륨</div>
-        <div>운동 시간</div>
+        <button type='button'>총 볼륨</button>
+        <button type='button'>운동 시간</button>
       </div>
       <Footer />
     </section>
--- src/routes/Summary/styles.ts
+++ src/routes/Summary/styles.ts
@@ -1,7 +1,20 @@
 import styled from 'styled-components'
 import colors from '../../styles/constants/colors'
 
-export const chartConatiner = styled.div`
+export const dountChartContainer = styled.div`
   width: 100%;
   display: flex;
+`
+export const barChartContainer = styled.div`
+  width: 100%;
+  display: flex;
+`
+export const yAxis = styled.div`
+  height: 178px;
+  padding: 0 10px;
+  border-right: 2px solid ${colors.FOCUS};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-align: center;
 `
